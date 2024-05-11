258d6e6d763a06e1a8e395337650e56e0d77ee92
Pataki Benjamin <169166402+pbeni01@users.noreply.github.com>
2024-05-11 20:55:11 +0200

    Add files via upload

--- templates/pages/kapcsolat.tpl.php
+++ templates/pages/kapcsolat.tpl.php
@@ -1,15 +1,17 @@
-<form action="http://localhost/beadando3/feldolgoz.php" method="post">
+<script type="text/javascript" src="js/main.js"></script>
+<form name="kapcsolat" action="http://localhost/beadando3/feldolgoz.php" onsubmit="return ellenoriz();" method="post">
     <div id="email">
-<label>E-mail:</label><input type="email" name="email" required> <br>
+<label>E-mail:</label><input type="email" id="email" name="email" required> <br>
 </div>
-<label>Név:</label><input type="text" name="nev" pattern="[A-Z,a-z, ]{8}[A-Z,a-z, ]*"
+<label>Név:</label><input type="text" id="nev" name="nev" pattern="[A-Z,a-z, ]{8}[A-Z,a-z, ]*"
 required><br>
 <p></p>
 <label for="">Telefonszám</label>
 <input type="tel" id="phone" name="phone" pattern="[0-9]{11}">
 <p></p>
 <h3>Megjegyzés:</h3>
-<textarea name="megjegyzes" rows="4" cols="50"></textarea>
+<textarea name="szoveg" id="szoveg" rows="4" cols="50"></textarea>
 <p></p>
-<label></label><input type="submit" value="Küldés">
+
+<input id="kuld" type="submit" value="Küldés">
 </form>
